ecfc5ad1aecfa8ddf27ffa2308eb97771331a9a6
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-20 00:31:40 +0530

    Javascript For in loops

---
+++ Arrays and For Loops/index.js
@@ -0,0 +1,20 @@
+//Arrays and for loops
+
+let names = ["Uttam", "Tommy", "Mark", "Brown", "James Bond", "Mobius", "Skylark"]
+let lennames = names.length
+console.log("length of names" , lennames);
+
+//write out all items in names
+
+console.log("log names forwards");
+for(let i = 0; i<lennames ; i++) { //for loop
+    console.log(names[i]);
+}
+//iterate through each element of array
+
+//write out all items in backwards
+
+console.log("log names backwards");
+for (let i = lennames-1; i >= 0; i--) {
+    console.log(names[i]);
+}
