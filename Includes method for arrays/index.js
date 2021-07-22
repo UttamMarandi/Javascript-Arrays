fb4640e9ac9fd295cba7cdad4e0082e47e6c365a
Uttam Marandi <uttammarandi007@gmail.com>
2021-07-22 23:57:26 +0530

    includes() method for arrays

---
+++ Includes method for arrays/index.js
@@ -0,0 +1,22 @@
+//includes() method for arrays
+
+//includes() method checks if the element is present in array or not
+//return a boolear true if element is present
+//includes can take two parameter: a.the element that needs to be searched b.the index from which we start the search
+
+
+let movies = [ "The last samurai", "Edge of tomorrow","Mission Impossible", "Rain Man", "Collateral", "Top Gun", "Risky Business", "Tropic Thunder", "Last Mission"]
+
+let hasMission = movies.includes("Mission")
+console.log(hasMission);
+//returns false ..as icludes() require the entire element to be equal not a part of it
+
+let hasMissionM = movies.includes("Mission Impossible")
+console.log(hasMissionM);
+//returns true
+
+let hasMissionS = movies.includes("Mission Impossible" , 3)
+console.log(hasMissionS);
+//returns false as after index 3 there is no element that matches the passed string
+
+
