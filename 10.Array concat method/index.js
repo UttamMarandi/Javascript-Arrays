6ed383b147efc6dc7148f185f5d44183a7ee0a25
Uttam Marandi <uttammarandi007@gmail.com>
2022-03-31 10:06:23 +0530

    array concat, split and join revision

--- 10.Array concat method/index.js
+++ 10.Array concat method/index.js
@@ -2,50 +2,62 @@
 
 //concat() method helps to combine multiple arrays
 
-let a = [2,4,6,8]
-let b = [1,3,5]
+// 4 ways
+// for loop
+//for in loop
+// for each loop
+//a.concat(b) or [].concat(a,b)
+
+let a = [2, 4, 6, 8];
+let b = [1, 3, 5];
 
 //combine using for loop
 
-for(let i=0; i<a.length; i++) {
-   b.push(a[i])
-   
+for (let i = 0; i < a.length; i++) {
+  b.push(a[i]);
 }
-console.log(b);
+console.log("b outside for 1st", b);
 
 //combine using for in loop
 
-for (prop in a){
-    b.push(a[prop]) //prop is index, a[prop] is value in that index
-    console.log(b);
+for (prop in a) {
+  // prop represent the keys ... in this cases numbered indices
+  b.push(a[prop]); //prop is index, a[prop] is value in that index
+  console.log("b inside for", b);
 }
-console.log(b);
+console.log("b outside for 2nd", b);
 
 //combine using forEach()
 
-a.forEach(function(item){
-    b.push(item)
-})
-console.log(b);
+a.forEach(function (item) {
+  b.push(item);
+});
+console.log("b after forEach", b);
 
 //combine using concat() method
 
-a = [2,4,6,8]
-b = [1,3,5]
+a = [2, 4, 6, 8];
+b = [1, 3, 5];
 
-let c = a.concat(b)
-console.log("c", c);
+let c = a.concat(b);
+console.log("a after concat", a);
+console.log("b after concat", b);
+console.log("c after concat", c);
 
 //alternate ways
-a = [2,4,6,8]
-b = [1,3,5]
+a = [2, 4, 6, 8];
+b = [1, 3, 5];
 
-let d = [].concat(a,b)
-console.log("d", d);
+let d = [].concat(a, b);
+console.log("d after concat", d);
 
-let e = [].concat(a,b,c)
-console.log("d+c" , e);
+console.log("a before concat", a);
+console.log("b before concat", b);
+console.log("c before concat", c);
+
+let e = [].concat(b, a, c);
+console.log("d+c", e);
 
 //chain method use sort()
-e = [].concat(a,b,c).sort()
-console.log("sorted" , e);
+e = [].concat(b, a, c).sort();
+console.log("sorted", e);
--- 11.String split and array join/index.js
+++ 11.String split and array join/index.js
@@ -7,53 +7,54 @@
 //string.split()
 //split() method takes two parameters a. the character/str by which the sentence will be divided.
 
+let sentence = "I am Iron Man";
 
-let sentence = "I am Iron Man"
-
-let words = sentence.split(" ") //divide by single space
+let words = sentence.split(" "); //divide by single space
 console.log(words);
 
-let chalk = sentence.split("am")
+let chalk = sentence.split("am");
 console.log("divided by a", chalk); //also the divide character is omitted in resulting array. here "am" is removed
- 
+
+let testWords = sentence.split("k");
+console.log("divide by k", testWords); // k is not present in the sentence so the sentence is not divided
+
 //combine with sort method
 
-words = sentence.split(" ").sort() //to sort the words in alphabeticalorder
-console.log("sorted words", words); 
-//here resu;lt is [ I , Iron, Man, am] b.c am is smallcase . 
+words = sentence.split(" ").sort(); //to sort the words in alphabeticalorder
+console.log("sorted words", words);
+//here resu;lt is [ I , Iron, Man, am] b.c am is smallcase .
 //so sort() prioritizes capital over small letters
 
-words = sentence.toUpperCase().split(" ").sort() 
+words = sentence.toUpperCase().split(" ").sort();
 
-console.log("uppercase sorted" , words);
+console.log("uppercase sorted", words);
 
 //pass empty string as parameter to split
 console.log("pass empty string as parameter to split");
 
-words = sentence.split("")
-console.log(words); //as empty string is passed so no divide character..hence each letter gets splitted into single element
-
+words = sentence.split("");
+console.log(words); //as empty string is passed so no divide character..hence each letter gets splitted into single element/word
 
 //array.join()
 //basically opposite of string.split()
 //join parameter adds the passed parameter instead of removing it
-let hyphenated = words.join("")
+let hyphenated = words.join("");
 console.log("join()");
 console.log("original", hyphenated); //gets back to original string as empty string is passed
-hyphenated = words.join ("-")
+hyphenated = words.join("-");
 console.log("hyphenated", hyphenated); //hyphen is added bewteen letter of string including between spaces
-console.log(typeof(hyphenated));
+console.log("typeof hyphenated", typeof hyphenated);
 
 //sentence with no spaces
-sentence = "I am Iron Man"
-let owords = sentence.split(" ")
+sentence = "I am Iron Man";
+let owords = sentence.split(" ");
 console.log("New word", owords);
-console.log(sentence);
-owords = owords.join("")
+console.log("original sentence", sentence);
+owords = owords.join("");
 console.log("sentence with no spaces", owords);
 
 //split then sort then join
 console.log("split then sort then join");
-sentence = "I am Iron Man"
-let mission = sentence.split(" ").sort().join(" ") //join 
+sentence = "I am Iron Man";
+let mission = sentence.split(" ").sort().join(" "); //join
 console.log(mission);
